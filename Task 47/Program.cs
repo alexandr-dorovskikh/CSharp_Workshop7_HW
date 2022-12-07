a8202425e3506b3bccae321b36e6c8365d2da7eb
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-12-07 20:53:23 +0700

    Task 47 added

---
+++ Task 47/Program.cs
@@ -0,0 +1,31 @@
+﻿void FillRandomValuesInArray(ref double[,] NumsArray)
+{
+    for (int i = 0; i < NumsArray.GetLength(0); i++)
+        for (int j = 0; j < NumsArray.GetLength(1); j++)
+            NumsArray[i,j] = Math.Round(new Random().NextDouble(), 1, MidpointRounding.AwayFromZero) + new Random().Next(-9, 10);
+       
+    
+}
+
+void PrintArray(double[,] NumsArray)
+{
+    for (int i = 0; i < NumsArray.GetLength(0); i++)
+    {    
+        Console.Write($"{i}:[");
+        
+        for (int j = 0; j < NumsArray.GetLength(1); j++)
+            {
+                Console.Write($"{NumsArray[i,j]}");
+                if (j + 1 != NumsArray.GetLength(1)) Console.Write("\t"); 
+            }    
+        
+        Console.WriteLine("]");
+    }    
+}
+
+int m = 3;
+int n = 4;
+
+double[,] NumsArray = new double[m, n];
+FillRandomValuesInArray(ref NumsArray);
+PrintArray(NumsArray);
